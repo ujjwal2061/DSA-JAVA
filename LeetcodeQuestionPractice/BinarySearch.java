dd2d19da3200482b2575ad1b624448931fbd6c78
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-08-10 11:39:20 +0530

    leet code question

---
+++ LeetcodeQuestionPractice/BinarySearch.java
@@ -0,0 +1,26 @@
+package LeetcodeQuestionPractice;
+
+public class BinarySearch {
+    // leetcode 704
+    public static void main(String[] args) {
+        int [] nums = {-1,0,3,5,9,12};
+        int target=2;
+        int ans=search(nums, target);
+        System.out.println("Ans" +ans);
+    }
+      static int search(int[] nums, int target){
+        int s=0;
+        int e=nums.length-1;
+         while(s<=e){
+            int mid=s+(e-s)/2;
+            if(target<nums[mid]){
+                e=mid-1;
+            }else if(target>nums[mid]){
+                s=mid+1;
+            }else{
+                return mid;
+            }
+         }
+         return -1;
+     }
+}
